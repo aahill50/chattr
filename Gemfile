603a11527e33d0149ba3399cf53918967c4ef7df
Aaron Hill <aahill50@gmail.com>
2014-10-27 22:30:40 -0400

    Initial setup

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.4'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.2'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'bcrypt'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+group :development do
+  gem 'annotate'
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'byebug'
+  gem 'pry-rails'
+end
+
+group :test do
+  gem 'rspec-rails', '~> 3.0'
+  gem 'capybara'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chattr::Application.config.secret_key_base = 'd33d3bba071f1f5923c47e0c0704b7b372188f5cd42560e8d76e844057cb397792e47423ac9effbdf2e215ab922611ae64f2255f57b1a40ed4e08d071f1dbb99'
